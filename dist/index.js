26578742136cefb8bb1a5dbec12ccfb8364c8a6e
Nirmal Shaji Joseph <nirmalshaji789@gmail.com>
2023-01-19 11:47:45 +0530

    socket is not working

--- dist/index.js
+++ dist/index.js
@@ -18,6 +18,7 @@ const socket_io_1 = require("socket.io");
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 const server = http_1.default.createServer(app);
+app.options('*', (0, cors_1.default)());
 const io = new socket_io_1.Server(server, {
     cors: {
         origin: 'https://livins.audiograms.xyz'
